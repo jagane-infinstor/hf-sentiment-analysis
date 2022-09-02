2fcbe40236565600cd62b77ea6f87fe040356367
Jagane Sundar <jagane@infinstor.com>
2022-09-02 15:58:15 -0700

    added main py file

---
+++ sentiment_analysis.py
@@ -0,0 +1,28 @@
+import mlflow
+import infinstor_mlflow_plugin
+import boto3
+from mlflow.store.artifact.models_artifact_repo import ModelsArtifactRepository
+import tempfile
+import pandas as pd
+import pickle
+import json
+
+##tdir = tempfile.mkdtemp()
+##print('model directory=' + str(tdir))
+##ModelsArtifactRepository("models:/HFSentimentAnalysis/Production").download_artifacts(artifact_path="", dst_path=tdir)
+
+tdir = '/tmp/tmpz804gezc'
+model = mlflow.pyfunc.load_model(tdir)
+
+print('model=' + str(model))
+
+#inp = ['This is great weather', 'This is terrible weather']
+jsonarray = pickle.load(open('/home/jagane/Downloads/1565264790192365568', 'rb'))
+#for i in jsonarray:
+#  print(json.dumps(i))
+
+df = pd.DataFrame(jsonarray, columns=['text'])
+ii = model.predict(df)
+ii.reset_index()
+for index, row in ii.iterrows():
+  print("'" + row['text'] + "' sentiment=" + row['label'] + ", score=" + str(row['score']))
